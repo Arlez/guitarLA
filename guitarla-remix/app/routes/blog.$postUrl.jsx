2dafbe4a08b1e7259d9f1c54d301142eddfc66cb
Daniel Barrera <dbarrera.ap@gmail.com>
2023-08-24 12:33:58 -0400

    avance

--- guitarla-remix/app/routes/blog.$postUrl.jsx
+++ guitarla-remix/app/routes/blog.$postUrl.jsx
@@ -1,22 +1,12 @@
 import { useLoaderData } from '@remix-run/react'
 import { getPost } from '~/models/posts.server'
 import { formatearFecha } from '../utils/helper'
-import styles from '~/styles/blog.css'
 
 export function meta(){
     return [
       {
         title: 'GuitarLA - Nuestro Blog'
       }
-    ]
-}
-
-export function links(){
-    return [
-        {
-            rel: 'stylesheet',
-            href: styles
-        }
     ]
 }
 
@@ -35,7 +25,7 @@ export default function Post() {
     const post = useLoaderData()
     const {titulo, contenido, imagen, publishedAt} = post?.data[0]?.attributes
   return (
-    <article className='contenedor post mt-3'>
+    <article className='post mt-3'>
         <img className='imagen' src={imagen.data.attributes.url} alt={`imagen ${titulo}`}/>
         <div className='contenido'>
             <h3>{titulo}</h3>
--- guitarla-remix/app/routes/blog.jsx
+++ guitarla-remix/app/routes/blog.jsx
@@ -1,15 +1,5 @@
-import { useLoaderData} from '@remix-run/react'
-import Post from '~/components/post'
-import { getPosts } from '~/models/posts.server'
+import { Outlet} from '@remix-run/react'
 import styles from '~/styles/blog.css'
-
-export function meta(){
-  return [
-    {
-      title: 'GuitarLA - Nuestro Blog'
-    }
-  ]
-}
 
 export function links(){
   return [
@@ -20,25 +10,10 @@ export function links(){
   ]
 }
 
-export async function loader(){
-  const posts = await getPosts()
-  return posts?.data
-}
-
 function Blog() {
-  const posts = useLoaderData()
-
   return (
     <main className="contenedor">
-      <h2 className="headomg">Blog</h2>
-      <div className="blog">
-        {posts.map(post => (
-          <Post
-            key={post.id}
-            post={post.attributes}
-          />
-        ))}
-      </div>
+      <Outlet/>
     </main>
   )
 }
