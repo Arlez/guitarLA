28e11bc6d8a620cb2b3579616f698e5511c9b84c
Daniel Barrera <dbarrera.ap@gmail.com>
2023-08-25 17:09:05 -0400

    avance

--- guitarla-remix/app/routes/guitarras.$guitarraUrl.jsx
+++ guitarla-remix/app/routes/guitarras.$guitarraUrl.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useLoaderData } from "@remix-run/react"
 import {getGuitarra} from "~/models/guitarras.server"
 
@@ -23,8 +24,26 @@ export function meta({data}){
 }
 
 function Guitarra() {
+  const [cantidad, setCantidad] = useState(0)
   const guitarra = useLoaderData()
   const {nombre, descripcion, imagen, precio} = guitarra.data[0].attributes
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(cantidad < 1){
+      alert('selecciona una cantidad')
+      return
+    }
+    const guitarraSeleccionada = {
+      id: guitarra.data[0].id,
+      imagen: imagen.data.attributes.url,
+      nombre,
+      precio,
+      cantidad
+    }
+    console.log(guitarraSeleccionada)
+  } 
+
   return (
     <div className="guitarra">
       <img className="imagen" src={imagen.data.attributes.url} alt={`imagen ${nombre}`}/>
@@ -32,6 +51,26 @@ function Guitarra() {
         <h3>{nombre}</h3>
         <p className="texto">{descripcion}</p>
         <p className="precio">${precio}</p>
+
+        <form onSubmit={handleSubmit} className="formulario">
+          <label htmlFor="cantidad">Cantidad</label>
+
+          <select 
+            onChange={e=>setCantidad(+e.target.value)}
+            id="cantidad"
+          >
+            <option value="0">-- Seleccione --</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+
+          <input type="submit" value="Agregar al carrito"/>
+
+        </form>
+
       </div>
     </div>
   )
